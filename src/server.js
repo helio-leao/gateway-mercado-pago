2986d811280f94d390d8fcedd67244b0e5b678e6
Helio Leao <hmtleao@hotmail.com>
2025-02-18 15:09:22 -0300

    add webhook endpoint to server for handling incoming requests

--- src/server.js
+++ src/server.js
@@ -6,5 +6,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello world!" });
 });
 
+app.get("/webhook", (req, res) => {
+  console.log("deu delicinha", req.body);
+  res.json({ message: "Hello world!" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
